2b2a7ab859876495d8c2f111c39fd944e23866a3
Anton <kondratov247@mail.ru>
2023-04-25 21:54:59 +0700

    Realization of a two-dimensional vector

---
+++ engine/math/Vec2D.h
@@ -0,0 +1,51 @@
+#ifndef SHOOTER_VEC2D_H
+#define SHOOTER_VEC2D_H
+
+#include <array>
+
+class Vec2D {
+    public:
+        Vec2D() = default;
+
+        Vec2D( const Vec2D &vec );
+
+        explicit Vec2D( double x, double y = 0.0 );
+
+        Vec2D &operator=( const Vec2D & ) = default;
+
+        [[nodiscard]] double x() const { return _arr_point[0]; }
+        [[nodiscard]] double y() const { return _arr_point[1]; }
+
+        [[nodiscard]] Vec2D operator-() const;
+
+        // Boolean operations
+        bool operator==( const Vec2D &vec ) const;
+        bool operator!=( const Vec2D &vec ) const;
+
+        // Accessing elements
+        double operator[]( size_t d );
+
+        [[nodiscard]] Vec2D operator+( const Vec2D &vec ) const;
+        [[nodiscard]] Vec2D operator-( const Vec2D &vec ) const;
+
+        [[nodiscard]] double dot( const Vec2D &vec ) const; // Returns dot product
+
+        // Operations with numbers
+        [[nodiscard]] Vec2D operator*( double number ) const;
+        [[nodiscard]] Vec2D operator/( double number ) const;
+
+        // Other useful methods
+        [[nodiscard]] double sqrAbs() const; // Returns squared vector length
+        [[nodiscard]] double abs() const; // Returns vector length
+        [[nodiscard]] Vec2D normalized() const; // Returns normalized vector without changing
+
+        static void test();
+
+    private:
+        static const unsigned short dimension = 2;
+        std::array<double, dimension> _arr_point{};
+
+        static bool isNear( double a, double b );
+};
+
+#endif //SHOOTER_VEC2D_H
