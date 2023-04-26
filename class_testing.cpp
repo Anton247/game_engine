096fc5815df2bbf6d406244811b3102ddbbff6fb
Anton <kondratov247@mail.ru>
2023-04-26 21:19:05 +0700

    Add Vec3D

--- class_testing.cpp
+++ class_testing.cpp
@@ -1,4 +1,5 @@
 #include "engine/math/Vec2D.h"
+#include "engine/math/Vec3D.h"
 #include <iostream>
 
 using namespace std;
@@ -10,7 +11,9 @@ int main(){
     Vec2D::test();
     cout<<"Test: Vec2D - OK\n";
 
-    
+    cout<<"Test: Vec3D - START\n";
+    Vec3D::test();
+    cout<<"Test: Vec3D - OK\n";
 
 
 }
