b2c400e883f71a16280f6fe31e0eb0b0de9e1e5f
Anton <kondratov247@mail.ru>
2023-04-26 20:34:35 +0700

    class testing

---
+++ class_testing.cpp
@@ -0,0 +1,16 @@
+#include "engine/math/Vec2D.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"class testing"<<endl;
+
+    cout<<"Test: Vec2D - START\n";
+    Vec2D::test();
+    cout<<"Test: Vec2D - OK\n";
+
+    
+
+
+}
